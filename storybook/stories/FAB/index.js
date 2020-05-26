856861ddee85ffa5a1801b4cdaa5be3f2bf1aac5
Chacko <chackop@gmail.com>
2020-05-26 19:16:59 +0100

    floating action button

--- storybook/stories/FAB/index.js
+++ storybook/stories/FAB/index.js
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, Text, View, TouchableNativeFeedback } from 'react-native'
 
 import Icon from 'react-native-vector-icons/MaterialIcons'
 
 const Fab = ({ onPress }) => {
+    const ripple = TouchableNativeFeedback.Ripple('#AAF', false)
+    const [pressed, setPressed] = useState(false)
+    const elevation = pressed ? 13 : 6
+
     return (
-        <TouchableNativeFeedback style={styles.fab} onPress={onPress}>
-            <Icon name='add-shopping-cart' color='black' size={24} />
-        </TouchableNativeFeedback>
+        <View style={[styles.fab, { elevation }]}>
+            <TouchableNativeFeedback
+                onPressIn={() => setPressed(true)}
+                onPressOut={() => setPressed(false)}
+                background={ripple} onPress={onPress}>
+                <View style={styles.iconContainer}>
+                    <Icon name='add-shopping-cart' color='black' size={24} />
+                </View>
+            </TouchableNativeFeedback>
+        </View>
     )
 }
 
@@ -20,6 +31,13 @@ const styles = StyleSheet.create({
         borderRadius: 28,
         elevation: 6,
         backgroundColor: 'white',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    iconContainer: {
+        width: 56,
+        height: 56,
+        borderRadius: 28,
         justifyContent: 'center',
         alignItems: 'center',
     }
