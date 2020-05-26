598b74578d875a42b9979b779481e8770ed94f03
Chacko <chackop@gmail.com>
2020-05-26 13:19:54 +0100

    simple elevation check

--- storybook/stories/index.js
+++ storybook/stories/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, Image } from 'react-native';
 
 import { storiesOf } from '@storybook/react-native';
 import { action } from '@storybook/addon-actions';
@@ -8,6 +8,7 @@ import { linkTo } from '@storybook/addon-links';
 import Button from './Button';
 import CenterView from './CenterView';
 import Welcome from './Welcome';
+import { catchClause } from '@babel/types';
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
@@ -23,3 +24,77 @@ storiesOf('Button', module)
       <Text>😀 😎 👍 💯</Text>
     </Button>
   ));
+
+storiesOf('Elevation', module)
+  .add('View', () => (
+    <View style={{
+      width: 350,
+      height: 350,
+      margin: 40,
+      elevation: 5,
+      backgroundColor: 'white',
+      borderRadius: 20
+    }} />
+  ))
+  .add('Text', () => (
+    <Text style={{
+      margin: 30,
+      elevation: 5,
+      fontSize: 50,
+      backgroundColor: 'white',
+      borderRadius: 20
+    }}>
+      Chacs
+    </Text>
+  ))
+  .add('Rounded Image', () => (
+    <View style={{
+      width: 350,
+      height: 350,
+      margin: 40,
+      elevation: 5,
+      backgroundColor: 'white',
+      borderRadius: 20
+    }}>
+      <Image
+        source={require('../../assets/images/robot-dev.png')}
+        style={{
+          width: 100,
+          height: 100,
+          borderRadius: 20,
+        }} />
+    </View>
+  ))
+  // .add('Card Parent Child', () => (
+  //   <View>
+  //     <Card elevation={5}>
+  //       <Card elevation={10} />
+  //     </Card>
+  //     <Card elevation={6}>
+  //       <Card elevation={2} />
+  //     </Card>
+  //   </View>
+  // ))
+  // .add('Card test with 3', () => (
+  //   <View>
+  //     <Card elevation={1} />
+  //     <Card elevation={15} />
+  //     <Card elevation={5} />
+  //   </View>
+  // ))
+  // .add('Card test one', () => (
+  //   <Card elevation={1} />
+  // ))
+  // .add('Image', () => (
+  //   <Image
+  //     source={require('../../assets/images/robot-dev.png')}
+  //     style={{
+  //       margin: 30,
+  //       elevation: 5,
+  //       width: 350,
+  //       height: 350,
+  //       fontSize: 50,
+  //       backgroundColor: 'white',
+  //     }} />
+  // ))
+
